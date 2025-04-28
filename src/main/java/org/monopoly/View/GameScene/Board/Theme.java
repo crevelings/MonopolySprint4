9f64be2f26a82d968ca5a503ba02bb4d20e7bcc3
Sean Creveling <crevelings@moravian.edu>
2025-04-28 00:41:24 -0400

    Lot's of javadocs

--- src/main/java/org/monopoly/View/GameScene/Board/Theme.java
+++ src/main/java/org/monopoly/View/GameScene/Board/Theme.java
@@ -1,5 +1,9 @@
 package org.monopoly.View.GameScene.Board;
 
+/**
+ *  Enumeration of available themes.
+ * @author crevelings
+ */
 public enum Theme {
     NORMAL("Normal", ""),
     CLASSIC("Classic", "classic-theme.css"),
@@ -10,19 +14,27 @@ public enum Theme {
     private final String displayName;
     private final String stylesheetPath;
 
+    /**
+     * Constructor
+     * @author crevelings
+     */
     Theme(String displayName, String stylesheetPath) {
         this.displayName = displayName;
         this.stylesheetPath = stylesheetPath;
     }
 
-    public String getDisplayName() {
-        return displayName;
-    }
-
+    /**
+     * Returns the path to the CSS file for the theme
+     * @author crevelings
+     */
     public String getStylesheetPath() {
         return "/styles/" + stylesheetPath;  // Assuming CSS files are in resources/styles/
     }
 
+    /**
+     * Returns the display name of the theme
+     * @author crevelings
+     */
     @Override
     public String toString() {
         return displayName;
